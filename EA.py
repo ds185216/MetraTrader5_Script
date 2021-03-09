032611e3640c2b32cdc51252bb3b581d43d2e7fd
Darryl Smith <ds185216@gmail.com>
2021-03-09 17:13:39 +1100

    Add files via upload
    bug fixes with invalid database length

--- EA.py
+++ EA.py
@@ -10,8 +10,8 @@ from sklearn.linear_model import LinearRegression
 
 
 def open_order(buy_sell):
-	bid = mt5.symbol_info(sym).bid
-	ask = mt5.symbol_info(sym).ask
+	bid = round(mt5.symbol_info(sym).bid, mt5.symbol_info(sym).digits)
+	ask = round(mt5.symbol_info(sym).ask, mt5.symbol_info(sym).digits)
 	if bid == 0:
 		DF = pd.DataFrame(mt5.copy_ticks_from(sym, (dt.today()), 1, mt5.COPY_TICKS_ALL))
 		if len (DF) > 0:
@@ -28,6 +28,9 @@ def open_order(buy_sell):
 		volume = round(int(((mt5.account_info().balance/100)*percent)/bid/mt5.symbol_info(sym).volume_step)*mt5.symbol_info(sym).volume_step, mt5.symbol_info(sym).digits)
 		if volume > mt5.symbol_info(sym).volume_max:
 			volume = mt5.symbol_info(sym).volume_max
+		#if volume < mt5.symbol_info(sym).volume_min:
+		#	symbols = symbols.delete(sym)
+		#	print ('symbol removed')
 		request = {
 			"action": mt5.TRADE_ACTION_DEAL,
 			"symbol": sym,
@@ -48,6 +51,8 @@ def open_order(buy_sell):
 		elif result.retcode == 10016:
 			print ('invalid stops', buy_sell, 'Seg:', seg, 'Ask:', ask)
 			print (request)
+		elif result.retcode == 10014:
+			print (sym, 'min/max error')
 		else:
 			print (buy_sell, 'Error:', result.retcode)
 			print (request)
@@ -71,61 +76,61 @@ mt5.initialize()
 
 while True:
 
-	#Open orders check
-	open_sym = []
-	for order in mt5.positions_get():
-		open_sym.append(order.symbol)
-		DF = pd.DataFrame(mt5.copy_ticks_from(order.symbol, (dt.today()), 100000, mt5.COPY_TICKS_ALL))
-		DF = DF[DF['time'] >= order.time]
-		if len(DF) > 0:
-			if order.type == 0:
-				change_sl = round(DF['bid'].max() - float(LR_Values.loc[order.symbol]['seg']), mt5.symbol_info(order.symbol).digits)
-				if order.profit > 0 and change_sl < order.price_open + mt5.symbol_info(sym).point:
-					change_sl = order.price_open + mt5.symbol_info(sym).point
-			if order.type == 1:
-				change_sl = round(DF['bid'].min() + float(LR_Values.loc[order.symbol]['seg']), mt5.symbol_info(order.symbol).digits)
-				if order.profit > 0 and change_sl > order.price_open - mt5.symbol_info(sym).point:
-					change_sl = order.price_open - mt5.symbol_info(sym).point
-			#Change sl
-			request = {
-				"action": mt5.TRADE_ACTION_SLTP,
-				"position": order.ticket,
-				"tp" : order.tp,
-				"sl" : change_sl,
-				}
-			result = mt5.order_send(request)
-			if result.retcode == 10009:
-				print (order.symbol, 'sl changed', change_sl)
-		else:
-			print ('empty dataframe')
-
-	#Check current EMA values
 	for sym in symbols:
+		#Open orders check
+		open_sym = []
+		for order in mt5.positions_get():
+			open_sym.append(order.symbol)
+			DF = pd.DataFrame(mt5.copy_ticks_from(order.symbol, (dt.today()), 100000, mt5.COPY_TICKS_ALL))
+			DF = DF[DF['time'] >= order.time]
+			if len(DF) > 0:
+				if order.type == 0:
+					change_sl = round(DF['bid'].max() - float(LR_Values.loc[order.symbol]['seg']), mt5.symbol_info(order.symbol).digits)
+					#if order.profit > 0 and change_sl < order.price_open + mt5.symbol_info(sym).point:
+					#	change_sl = order.price_open + mt5.symbol_info(sym).point
+				if order.type == 1:
+					change_sl = round(DF['bid'].min() + float(LR_Values.loc[order.symbol]['seg']), mt5.symbol_info(order.symbol).digits)
+					#if order.profit > 0 and change_sl > order.price_open - mt5.symbol_info(sym).point:
+					#	change_sl = order.price_open - mt5.symbol_info(sym).point
+				#Change sl
+				request = {
+					"action": mt5.TRADE_ACTION_SLTP,
+					"position": order.ticket,
+					"tp" : order.tp,
+					"sl" : change_sl,
+					}
+				result = mt5.order_send(request)
+				if result.retcode == 10009:
+					print (order.symbol, 'sl changed', change_sl)
+			else:
+				print ('empty dataframe')
+
+		#Check current EMA values
 		#DataFrame cleanup
-		DF = pd.DataFrame(mt5.copy_ticks_from(sym, (dt.today()), 100000, mt5.COPY_TICKS_ALL))
+		DF = pd.DataFrame(mt5.copy_ticks_from(sym, (dt.today()), 1000000, mt5.COPY_TICKS_ALL))
 		if len(DF) > 0 and sym not in open_sym:
 			seg = float(LR_Values.loc[sym]['seg'])
 			sample = LR_Values.loc[sym]['sample']
 			LinReg = LR_Values.loc[sym]['LinReg']
-			DF = DF.iloc[::-1]
 			DF.index = pd.to_datetime(DF['time'], unit='s')
 			DF = DF.drop(['volume', 'last', 'time_msc', 'flags', 'volume_real'], axis=1)
 			DF.index = pd.to_datetime(DF['time'], unit='s')
 			DF = DF.drop_duplicates(subset='time', keep="first")
-			DF = DF.resample(sample).fillna("ffill").dropna().drop('time', axis=1)			
-			y = pd.DataFrame(DF['ask'][-LinReg:])
-			X = (pd.DataFrame(range(LinReg)))
-			model = LinearRegression()
-			model.fit(X,y)
-			pred_A = model.predict([[LinReg+1]])
-			pred_B = model.predict([[LinReg+2]])
+			DF = DF.resample(sample).fillna("ffill").dropna().drop('time', axis=1)
+			if len(DF) >= LinReg:
+				y = pd.DataFrame(DF['ask'][-LinReg:])
+				X = (pd.DataFrame(range(LinReg)))
+				model = LinearRegression()
+				model.fit(X,y)
+				pred_A = model.predict([[LinReg+1]])
+				pred_B = model.predict([[LinReg+2]])
 
 
-			#Qualify for Buy/Sell
-			if pred_B > pred_A:
-				open_order('Buy')
-				break
+				#Qualify for Buy/Sell
+				if pred_B > pred_A:
+					open_order('Buy')
+					break
 
-			elif pred_B < pred_A:
-				open_order('Sell')
-				break
+				elif pred_B < pred_A:
+					open_order('Sell')
+					break
